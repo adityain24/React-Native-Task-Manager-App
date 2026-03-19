8a9f0168b133aae63209b19230826d0c046555fe
Aditya <aditya.in24@outlook.com>
2026-03-19 11:45:59 +0530

    Added Header and Empty Component FlatList

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet, ScrollView } from "react-native";
 import { getTasks, saveTasks, Task } from "../utils/storage";
 import AddTask from "../../components/AddTask";
 import TaskItem from "../../components/TaskItem";
@@ -16,24 +16,24 @@ export default function HomeScreen() {
     saveTasks(updated);
   };
 
-  // CREATE
+  // CREATE karna hai toh title string ko use karke ek naya task object banao, persist karke existing tasks ke saath add karo id : Date.now().toString() se generate kar sakte ho, completed default false rahega.
   const handleAdd = (title: string) => {
     persist([...tasks, { id: Date.now().toString(), title, completed: false }]);
   };
 
-  // UPDATE — toggle
+  // UPDATE : toggle karna hai toh map kar do, completed ko !completed replace kardo fir persist kar do
   const handleToggle = (id: string) => {
     persist(
       tasks.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t)),
     );
   };
 
-  // UPDATE — edit title
+  // UPDATE : edit title karna hai toh map kar do, title newTitle replace kardo fir persist kar do
   const handleEdit = (id: string, newTitle: string) => {
     persist(tasks.map((t) => (t.id === id ? { ...t, title: newTitle } : t)));
   };
 
-  // DELETE
+  // DELETE karna hai toh filter kar do aur persist kar do
   const handleDelete = (id: string) => {
     persist(tasks.filter((t) => t.id !== id));
   };
@@ -46,9 +46,14 @@ export default function HomeScreen() {
       </Text>
 
       <AddTask onAdd={handleAdd} />
-
       <FlatList
         data={tasks}
+        //Added Header Component
+        ListHeaderComponent={<Text style={styles.userlist}>Task List</Text>}
+        //Added Empty Component
+        ListEmptyComponent={
+          <Text style={styles.empty}>No tasks found in this list</Text>
+        }
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <TaskItem
@@ -86,5 +91,11 @@ const styles = StyleSheet.create({
     color: "#aaa",
     marginTop: 60,
     fontSize: 15,
+  },
+  userlist: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginLeft: 16,
+    marginBottom: 8,
   },
 });
